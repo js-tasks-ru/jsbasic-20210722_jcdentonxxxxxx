13285920f4263508097631ec580b31ad03903b36
Vitaly Kuzmenko <jcdentonxxxxxx@gmail.com>
2021-08-03 23:00:14 +0300

    module-3: 1-task, 2-task, 3-task, 4-task, 5-task done

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
-  // ваш код...
+  let result = users.filter(item => item.age <= age);
+  let str = '';
+  for (let i = 0; i < result.length; i++) {
+    str += result[i].name + ', ' + result[i].balance;
+    if (i < result.length - 1) {
+      str += '\n';
+    }
+  }
+
+
+  return str;
 }
