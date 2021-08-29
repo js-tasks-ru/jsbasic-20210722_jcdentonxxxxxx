bdf1ce60ac30678baf6a9705e15a60202f486350
Vitaly Kuzmenko <jcdentonxxxxxx@gmail.com>
2021-08-29 23:09:07 +0300

    module-8: task-1, task-2 done

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -26,7 +26,9 @@ export default class CartIcon {
       this.elem.classList.add('shake');
       this.elem.addEventListener('transitionend', () => {
         this.elem.classList.remove('shake');
-      }, {once: true});
+      }, {
+        once: true
+      });
 
     } else {
       this.elem.classList.remove('cart-icon_visible');
@@ -39,6 +41,45 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (this.elem.offsetWidth <= 0) {
+      return;
+    }
+    if (!this.initialBlock) {
+      this.initialBlock = {};
+      this.initialBlock.top = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+
+    if (window.innerWidth <= 767) {
+      this.elem.style.position = 'absolute';
+      this.elem.style.top = '';
+      this.elem.style.zIndex = '';
+      return;
+    }
+
+    if (window.pageYOffset > this.initialBlock.top) {
+      this.elem.style.position = 'fixed';
+      this.elem.style.top = 50 + 'px';
+      this.elem.style.zIndex = 1000;
+      let containerCheck = document.querySelector('.container');
+      let left = containerCheck.getBoundingClientRect().right + 20;
+      let right = document.documentElement.clientWidth - this.elem.offsetWidth - 10;
+
+
+      if (left > right) {
+        left = right;
+      }
+      this.elem.style.left = left + 'px';
+
+    } else {
+      this.elem.style.position = 'absolute';
+      this.elem.style.top = '';
+      this.elem.style.zIndex = '';
+      this.elem.style.left = '';
+    }
+
+
+
+
+
   }
 }
--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,91 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.render();
+
   }
+
+  render() {
+    this.elem = createElement(`
+      <div class="products-grid">
+
+      </div>
+    `);
+
+    this.addCards();
+
+  }
+  addCards() {
+    let productsGridInner = createElement(`
+        <div class="products-grid__inner">
+
+        </div>
+    `);
+
+    let filtredObjects = this.products;
+
+    if (!this.isEmpty(this.filters)) {
+
+      filtredObjects = this.products.filter((item) => {
+        let bool = [];
+        if (this.filters.vegeterianOnly) {
+          if (item.vegeterian) {
+            bool.push(true);
+          } else {
+            bool.push(false);
+          }
+        }
+
+        if (this.filters.maxSpiciness) {
+          if (item.spiciness <= this.filters.maxSpiciness) {
+            bool.push(true);
+          } else {
+            bool.push(false);
+          }
+        }
+
+        if (this.filters.category) {
+          if (item.category === this.filters.category) {
+            bool.push(true);
+          } else {
+            bool.push(false);
+          }
+        }
+
+        if ("noNuts" in this.filters) {
+          if (this.filters.noNuts) {
+            bool.push(!!item.nuts === false);
+          }
+        }
+
+        if (!bool.includes(false)) {
+          return true;
+        }
+
+
+      });
+    }
+
+
+    for (let i = 0; i < filtredObjects.length; i++) {
+      let productCard = new ProductCard(filtredObjects[i]);
+      productsGridInner.append(productCard.elem);
+    }
+    this.elem.append(productsGridInner);
+  }
+
+  updateFilter(filters) {
+    this.elem.querySelector('.products-grid__inner').remove();
+    Object.assign(this.filters, filters);
+    this.addCards();
+  }
+
+  isEmpty(obj) {
+    for (const key in obj) {
+      return false;
+    }
+    return true;
+  }
+
+
 }
